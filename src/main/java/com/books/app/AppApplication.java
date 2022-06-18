fb6a2a7ec3e4a694355c11676fd69ec202ffac8c
ojarumaru1212 <yoshiaki.ojarumaru@gmail.com>
2022-06-19 00:49:46 +0900

    war対応

--- src/main/java/com/books/app/AppApplication.java
+++ src/main/java/com/books/app/AppApplication.java
@@ -2,15 +2,22 @@ package com.books.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * SpringBoot起動クラス
  */
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppApplication.class);
 	}
 
 }
